6a6734059fa3df218746e340b233827fa4077746
Imran Baloch <imran_ku07@yahoo.com>
2014-01-04 23:24:32 +0400

    First Commit

---
+++ Simple.Microsoft.AspNet.Identity/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Simple.Microsoft.AspNet.Identity
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public virtual IDbSet<ApplicationUser> Users { get; set; }
+    }
+
+}
---
+++ Simple.Microsoft.AspNet.Identity/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+
+namespace Simple.Microsoft.AspNet.Identity
+{
+    public class ApplicationUser : IUser
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        public ApplicationUser(string userName)
+            : this()
+        {
+            UserName = userName;
+        }
+        public virtual string Id { get; set; }
+        public virtual string PasswordHash { get; set; }
+        public virtual string SecurityStamp { get; set; }
+        public virtual string UserName { get; set; }
+    }
+
+}
